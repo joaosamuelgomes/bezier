d98855c142158ea8af07425a3af2599083a4e097
joaosamuelgomes <joaosamuelg001@gmail.com>
2023-11-09 17:55:06 -0300

    Inserindo documentação

--- bezier.js
+++ bezier.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let points = [];
     let draggingPoint = null;
 
+    /* 
+    Esta função desenha um ponto no canvas. 
+    O ponto é representado por um círculo preenchido com o centro nas coordenadas (point.x, point.y) e um raio fixo de 5 pixels.
+    Matematicamente, isso é representado pela equação: (x - point.x)² + (y - point.y)² = raio².
+    */ 
     function drawPoint(point, color = 'black') {
         ctx.fillStyle = color;
         ctx.beginPath();
@@ -11,6 +16,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
         ctx.fill();
     }
 
+    /*
+    Esta função desenha uma linha reta no canvas.
+    A linha começa no ponto start e termina no ponto end, com uma cor especificada pelo parâmetro color ou azul por padrão.
+    A equação matemática de uma linha reta entre dois pontos (x1, y1) e (x2, y2) pode ser expressa como y - y1 = ((y2 - y1) / (x2 - x1)) * (x - x1).
+    */
     function drawLine(start, end, color = 'blue') {
         ctx.strokeStyle = color;
         ctx.beginPath();
@@ -19,6 +29,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         ctx.stroke();
     }
 
+    /*
+    Esta função desenha uma curva de Bézier quadrática.
+    A curva começa no ponto start, passa pelo ponto de controle control, e termina no ponto end. A cor da curva é determinada pelo parâmetro color ou verde por padrão.
+    A curva de Bézier quadrática é definida pela equação paramétrica:
+    B(t) = (1 - t)² * start + 2 * (1 - t) * t * control + t² * end, onde t varia de 0 a 1.
+    */
     function drawQuadraticBezier(start, control, end, color = 'green') {
         ctx.strokeStyle = color;
         ctx.beginPath();
@@ -26,7 +42,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
         ctx.quadraticCurveTo(control.x, control.y, end.x, end.y);
         ctx.stroke();
     }
-    
+
+    /*
+    Desenha uma curva de Bézier cúbica no canvas.
+    A curva começa no ponto start, é guiada pelos pontos de controle control1 e control2, e termina no ponto end. A cor é definida pelo parâmetro color ou vermelho por padrão.
+    A curva de Bézier cúbica é dada pela equação:
+    B(t) = (1 - t)³ * start + 3 * (1 - t)² * t * control1 + 3 * (1 - t) * t² * control2 + t³ * end.
+    */
     function drawCubicBezier(start, control1, control2, end, color = 'red') {
         ctx.strokeStyle = color;
         ctx.beginPath();
@@ -38,27 +60,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-        // Desenhar a linha reta
         if (points.length >= 2) {
             drawLine(points[0], points[1]);
         }
     
-        // Desenhar a curva quadrática de Bézier com o terceiro ponto como controle
         if (points.length >= 3) {
             drawQuadraticBezier(points[0], points[1], points[2]);
         }
     
-        // Desenhar a curva cúbica de Bézier com o terceiro e quarto pontos como controles
         if (points.length === 4) {
             drawCubicBezier(points[0], points[1], points[2], points[3]);
         }
     
-        // Desenhar os pontos
         points.forEach(point => {
             drawPoint(point);
         });
     }
 
+    /*
+    Esta função verifica se um ponto com coordenadas (x, y) está próximo de um ponto alvo target dentro de um determinado raio.
+    É uma verificação de proximidade que usa o teorema de Pitágoras para calcular a distância entre dois pontos.
+    Matematicamente, isso é verificado pela desigualdade (x - target.x)² + (y - target.y)² < raio².
+    */
     function isNearPoint(x, y, target, radius = 5) {
         return Math.sqrt((target.x - x) ** 2 + (target.y - y) ** 2) < radius;
     }
@@ -68,8 +91,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
         
+        /*
+        Atribui o ponto a ser arrastado caso o usuário esteja clicando proximo a um ponto definido.
+        */
         draggingPoint = points.find(point => isNearPoint(x, y, point));
 
+        /*
+        Permite adicionar novos pontos caso o usuário esteja clicando em areas vazias do canvas e o numero de pontos for menor que 4.
+        */
         if (!draggingPoint && points.length < 4) {
             const newPoint = { x, y };
             points.push(newPoint);
--- readme.md
+++ readme.md
@@ -33,6 +33,44 @@ O visualizador de Curvas de Bézier é ideal para estudantes e profissionais que
     - Arraste os pontos para ajustar a forma das curvas.
     - Observe as curvas se ajustando em tempo real à medida que você move os pontos.
 
+## Desenho Interativo de Curvas de Bézier em Canvas
+
+Esta seção da documentação oferece detalhes sobre os processos matemáticos e as funções em JavaScript utilizadas para desenhar interativamente pontos, linhas e curvas de Bézier dentro de um elemento canvas.
+
+### Funções
+
+- `drawPoint(point, color)`: Cria um círculo preenchido que representa um ponto no canvas, baseando-se na equação matemática de um círculo.
+
+- `drawLine(start, end, color)`: Renderiza uma linha reta entre dois pontos, seguindo a representação matemática de uma equação linear que passa por esses pontos.
+
+- `drawQuadraticBezier(start, control, end, color)`: Desenha uma curva quadrática de Bézier, definida pelos pontos inicial, de controle e final. A equação da curva é paramétrica, misturando os pontos com base no parâmetro que varia de 0 a 1.
+
+- `drawCubicBezier(start, control1, control2, end, color)`: Semelhante à quadrática, esta funcionalidade desenha uma curva cúbica de Bézier. Ela envolve um ponto de controle adicional, fornecendo um maior grau de curvatura.
+
+- `isNearPoint(x, y, target, radius)`: Usa o teorema de Pitágoras para determinar se uma coordenada dada está dentro de um raio especificado de um ponto alvo.
+
+- `draw()`: Função que limpa o canvas e invoca condicionalmente o desenho de linhas e curvas de Bézier com base no número de pontos definidos.
+
+### Eventos do Mouse
+
+- `mousedown`, `mousemove`, `mouseup`, `mouseleave`: Estes eventos lidam com as interações do usuário, permitindo o movimento de pontos existentes ou a adição de novos para definir as curvas.
+
+### Inicialização
+
+- `DOMContentLoaded`: Garante que a lógica de desenho seja inicializada apenas após o carregamento completo do conteúdo da página, incluindo o DOM.
+
+- `reloadButton`: Associado a um botão para recarregar a aplicação inteira, redefinindo o estado do desenho.
+
+### Explicação do Desenho Interativo
+
+Esta aplicação permite que os usuários desenhem curvas de Bézier de forma interativa, colocando e arrastando pontos no canvas. Aqui está um resumo do processo de desenho:
+
+1. **Linha Reta**: Quando dois pontos são colocados, uma linha reta é desenhada.
+2. **Curva de Bézier Quadrática**: Com três pontos, a aplicação renderiza uma curva de Bézier quadrática que passa pelos pontos inicial e final, com o segundo ponto atuando como ponto de controle para definir a forma da curva.
+3. **Curva de Bézier Cúbica**: Quatro pontos permitem a criação de uma curva de Bézier cúbica, com o terceiro ponto se tornando o segundo ponto de controle, possibilitando curvas mais complexas e nuances.
+
+Use o canvas para desenhar livremente e compreender a beleza geométrica das curvas de Bézier!
+
 ## Equipe de Desenvolvimento
 
 - João Samuel Gomes
